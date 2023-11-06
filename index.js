7a1e4f3159f15465e29cbb7e4cfb8e2682430f6d
abdullahal5 <abdullahalfahin183@gmail.com>
2023-11-06 09:53:44 -0800

    post method complete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors())
@@ -30,12 +30,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const jobsCollection = client.db('jobsDB').collection('jobs')
+
+    app.post('/jobs', async(req, res) =>{
+        try{
+            const body = req.body;
+        console.log(body)
+        const result = await jobsCollection.insertOne(body)
+        res.send(result)
+        }
+        catch(err){
+            console.log(err)
+        }
+    })
+    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
